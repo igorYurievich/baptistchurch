d5aadc268151488e33d289ba814381025c6f5224
IgorLipskiy <igoryurievich1@gmail.com>
2024-08-28 02:04:38 +0200

    Чистка js файла от комментариев

--- css/animation.js
+++ css/animation.js
@@ -2,41 +2,40 @@ document.addEventListener("DOMContentLoaded", function() {
     const preloader = document.getElementById('preloader');
     const preloaderKey = 'preloaderShown';
 
-    // Функция для установки значения в sessionStorage
+    
     function setSessionStorage(key, value) {
         sessionStorage.setItem(key, value);
     }
 
-    // Функция для получения значения из sessionStorage
+   
     function getSessionStorage(key) {
         return sessionStorage.getItem(key);
     }
 
-    // Функция для определения, был ли переход с другой страницы (внешней)
+   
     function isExternalNavigation() {
-        // Если referrer пустой или не соответствует текущему хосту, считаем, что переход внешний
+        
         return !document.referrer || !document.referrer.includes(window.location.hostname);
     }
 
     if (isExternalNavigation()) {
-        // Если это внешний переход, показываем прелоадер только если он не был показан в текущей сессии
+   
         if (!getSessionStorage(preloaderKey)) {
             setTimeout(() => {
                 preloader.classList.add('hidden');
-            }, 3000); // 3 секунды
-
+            }, 3000); 
             setTimeout(() => {
                 preloader.style.display = 'none';
-            }, 4000); // 4 секунды для полного скрытия
+            }, 4000); 
 
-            // Устанавливаем значение в sessionStorage, чтобы прелоадер не показывался при последующих посещениях в этой сессии
+            
             setSessionStorage(preloaderKey, 'true');
         } else {
-            // Если значение уже есть, сразу скрываем прелоадер
+        
             preloader.style.display = 'none';
         }
     } else {
-        // Если это внутренний переход, сразу скрываем прелоадер
+       
         preloader.style.display = 'none';
     }
 });
