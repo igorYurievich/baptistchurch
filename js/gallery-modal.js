b50b59737ff1c95758021d0de12329a7be1180bb
IgorLipskiy <igoryurievich1@gmail.com>
2024-08-30 00:36:45 +0200

    Импорт фотографии крещения 2023 года и первый билд модальных окон

---
+++ js/gallery-modal.js
@@ -0,0 +1,36 @@
+// gallery-modal.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    var modal = new bootstrap.Modal(document.getElementById('photoModal'));
+    var modalImage = document.getElementById('modalImage');
+    var saveImageLink = document.getElementById('saveImage');
+    var images = document.querySelectorAll('[data-bs-image]');
+    var currentIndex;
+
+    images.forEach((image, index) => {
+        image.addEventListener('click', function() {
+            modalImage.src = image.src;
+            saveImageLink.href = image.src;  // Устанавливаем ссылку на изображение для загрузки
+            currentIndex = index;
+            modal.show();
+        });
+    });
+
+    document.getElementById('prevImage').addEventListener('click', function() {
+        if (currentIndex > 0) {
+            currentIndex--;
+            modalImage.src = images[currentIndex].src;
+            saveImageLink.href = images[currentIndex].src;  // Обновляем ссылку при смене изображения
+        }
+    });
+
+    document.getElementById('nextImage').addEventListener('click', function() {
+        if (currentIndex < images.length - 1) {
+            currentIndex++;
+            modalImage.src = images[currentIndex].src;
+            saveImageLink.href = images[currentIndex].src;  // Обновляем ссылку при смене изображения
+        }
+    });
+});
+
+
